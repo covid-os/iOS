e338fdbdb469a1db7921abd943bab7712020acf4
Imthath M <imthath.m@icloud.com>
2020-04-03 09:49:32 +0530

    Initial Commit

---
+++ CovidCases/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidCases
+//
+//  Created by Imthath M on 03/04/20.
+//  Copyright © 2020 Imthath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
